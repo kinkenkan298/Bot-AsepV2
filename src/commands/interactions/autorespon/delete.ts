6cbab0389a93dc7d95452fb4d5a4285bfaaae48f
kinkenkan298 <akin@programmer.net>
2025-02-10 14:25:15 +0700

    feat(autorespon): bikin fitur hapus pesan otomatis

--- src/commands/interactions/autorespon/delete.ts
+++ src/commands/interactions/autorespon/delete.ts
@@ -1,6 +1,21 @@
+import AutoresponModel from "#/structures/schemas/guilds/AutoresponModel.js";
+import { AsepEmbed } from "#/structures/utils/classes/AsepEmbed.js";
 import { Cooldown, CooldownType } from "@slipher/cooldown";
 import ms from "ms";
-import { CommandContext, Declare, SubCommand } from "seyfert";
+import {
+  CommandContext,
+  createStringOption,
+  Declare,
+  Options,
+  SubCommand,
+} from "seyfert";
+
+const options = {
+  pesan: createStringOption({
+    description: "Pesan apa yang ingin dihapus?",
+    required: true,
+  }),
+};
 
 @Declare({
   name: "delete",
@@ -14,10 +29,46 @@ import { CommandContext, Declare, SubCommand } from "seyfert";
     default: 1,
   },
 })
+@Options(options)
 export default class DeleteSubcommand extends SubCommand {
-  public override run(ctx: CommandContext) {
-    ctx.write({
-      content: "Delete",
-    });
+  public override async run(ctx: CommandContext<typeof options>) {
+    const { client, interaction, guildId, options } = ctx;
+    if (!interaction?.replied) await ctx.deferReply();
+
+    const { pesan } = options;
+    const embed = new AsepEmbed({}, client);
+    try {
+      const fetchData = await AutoresponModel.findOne({
+        guildId,
+        "autorespon.pesan": pesan,
+      });
+      if (!fetchData) {
+        await ctx.editOrReply({
+          embeds: [
+            embed
+              .setType("error")
+              .setTitle("Pesan yang ingin dihapus tidak ada !")
+              .setDescription(
+                `Pesan: ${pesan}\nTolong periksa ulang pesan nya!`,
+              ),
+          ],
+        });
+        return;
+      }
+      await AutoresponModel.findOneAndUpdate(
+        { guildId },
+        { $pull: { autorespon: { pesan } } },
+      );
+      await ctx.editOrReply({
+        embeds: [
+          embed
+            .setType("success")
+            .setTitle("Pesan otomatis sudah dihapus!!")
+            .setDescription(`Pesan: ${pesan}!`),
+        ],
+      });
+    } catch (e) {
+      client.logger.error(e);
+    }
   }
 }
