b9f073e3d9c9986f02877edd54c162f55abf33bf
kinkenkan298 <akin@programmer.net>
2025-02-13 10:39:30 +0700

    fix: autorespon juga merespon chat yang di channel khusus

--- src/structures/listeners/user/Autorespon.ts
+++ src/structures/listeners/user/Autorespon.ts
@@ -1,19 +1,25 @@
 import AutoresponModel from "#asep/schemas/guilds/AutoresponModel.js";
+import ChatAIModel from "#asep/structures/schemas/user/ChatAIModel.js";
 import { Message, UsingClient } from "seyfert";
 
 export async function AutoresponListener(
   client: UsingClient,
   message: Message,
 ) {
-  const { guildId, content } = message;
+  const { guildId, content, channelId } = message;
   const msg = content.toLowerCase();
 
   try {
     const fetchData = await AutoresponModel.findOne({ guildId });
-    if (!fetchData) return;
-    for (const data of fetchData.autorespon) {
-      const { pesan, balesan } = data;
-      if (msg === pesan) message.reply({ content: balesan });
+    const findChannel = await ChatAIModel.findOne({ guildId })
+    if (!fetchData || !findChannel || findChannel.channels.length === 0) return;
+    for (const ChatChannel of findChannel.channels) {
+      if (ChatChannel.channelId !== channelId) {
+        for (const data of fetchData.autorespon) {
+          const { pesan, balesan } = data;
+          if (msg === pesan) message.reply({ content: balesan });
+        }
+      }
     }
   } catch (e) {
     client.logger.error(e);
