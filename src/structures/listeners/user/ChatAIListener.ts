83fa9dc0f7be980c0b7846cfb4f8674ad2473794
kinkenkan298 <akin@programmer.net>
2025-02-14 01:55:19 +0700

    style: beberapa di prettier

--- src/structures/listeners/user/ChatAIListener.ts
+++ src/structures/listeners/user/ChatAIListener.ts
@@ -1,81 +1,88 @@
 import { Message, UsingClient } from "seyfert";
 import ChatAIModel from "#asep/structures/schemas/user/ChatAIModel.js";
-import { GoogleGenerativeAI, GenerationConfig, SafetySetting, HarmCategory, HarmBlockThreshold, Content } from '@google/generative-ai'
+import {
+  GoogleGenerativeAI,
+  GenerationConfig,
+  SafetySetting,
+  HarmCategory,
+  HarmBlockThreshold,
+  Content,
+} from "@google/generative-ai";
 
 const CONFIG_AI: GenerationConfig = {
   temperature: 0.9,
   topK: 1,
   topP: 1,
-  maxOutputTokens: 2048
-}
+  maxOutputTokens: 2048,
+};
 const SAFETY_SETTINGS: SafetySetting[] = [
   {
     category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
   },
   {
     category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
   },
   {
     category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
   },
   {
     category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
   },
-]
+];
 
 export async function ChatAIListerner(client: UsingClient, message: Message) {
   try {
     const { channelId, guildId, content, author } = message;
     const messageMaxContentLength = 2000;
-    const findChannel = await ChatAIModel.findOne({ guildId })
+    const findChannel = await ChatAIModel.findOne({ guildId });
     if (!findChannel || findChannel.channels.length === 0) return;
     for (const ChatChannel of findChannel.channels) {
       if (ChatChannel.channelId !== channelId) return;
       if (content.length > messageMaxContentLength) return;
       if (ChatChannel.authorId !== author.id) return;
-      await client.channels.typing(channelId)
       let prevMessages = await client.messages.list(channelId, {
-        limit: 15
-      })
-      prevMessages.reverse()
-      let historyChat: Content[] = []
+        limit: 15,
+      });
+      prevMessages.reverse();
+      let historyChat: Content[] = [];
       prevMessages.forEach((msg) => {
         if (msg.author.id !== client.me.id && msg.author.bot) return;
         if (msg.author.id === client.me.id) {
-          if (msg.content.includes('@')) return;
+          if (msg.content.includes("@")) return;
           historyChat.push({
             role: "model",
-            parts: [{ text: `${msg.content}` }]
-          })
+            parts: [{ text: `${msg.content}` }],
+          });
         } else {
           if (msg.author.id === author.id) {
             historyChat.push({
               role: "user",
-              parts: [{ text: `${msg.content}` }]
-            })
+              parts: [{ text: `${msg.content}` }],
+            });
           }
         }
-      })
+      });
+      await client.channels.typing(channelId);
       const GeminiAI = new GoogleGenerativeAI(process.env.APIKEY_GEMINI!);
       const modelAI = GeminiAI.getGenerativeModel({
-        model: "gemini-1.5-pro"
-      })
+        model: "gemini-1.5-pro",
+      });
       const chats = modelAI.startChat({
         safetySettings: SAFETY_SETTINGS,
         generationConfig: CONFIG_AI,
-        history: historyChat
-      })
+        history: historyChat,
+      });
       const chatting = await chats.sendMessage(content);
       const respon = chatting.response.text();
       for (let i = 0; i < respon.length; i += messageMaxContentLength) {
-        const chunkMessage = respon.substring(i, i + messageMaxContentLength)
-        await message.reply({
-          content: chunkMessage
-        })
+        const chunkMessage = respon.substring(i, i + messageMaxContentLength);
+        await message.write({
+          content: chunkMessage,
+        });
       }
     }
   } catch (e) {
