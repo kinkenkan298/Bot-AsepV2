a6b8fdd4d49d959d8f623b929a15b0bf73bcaf6a
kinkenkan298 <akin@programmer.net>
2025-02-26 16:24:03 +0700

    feat(reactrole): udah bisa hapus role dari react

--- src/events/ReactionsDelete.ts
+++ src/events/ReactionsDelete.ts
@@ -1,8 +1,33 @@
+import ReactRoleModel from "#asep/structures/schemas/guilds/ReactRoleModel.js";
 import { createEvent } from "seyfert";
 
 export default createEvent({
   data: { name: "messageReactionRemove" },
   run: async (reactions, client) => {
-    console.log(reactions);
+    if (!reactions.guildId) return;
+
+    const { guildId, messageId, userId } = reactions;
+
+    let reactModel;
+    try {
+      reactModel = await ReactRoleModel.findOne({
+        guildId,
+        messageId,
+        emoji: reactions.emoji.id || reactions.emoji.name,
+      });
+    } catch (e) {
+      client.logger.error(e);
+    }
+    if (!reactModel) return;
+
+    const roles = await client.roles.list(guildId);
+    if (!roles) return;
+
+    const member = client.cache.guilds?.get(guildId);
+    for (const role of roles) {
+      if (reactModel.roleId === role.id) {
+        await member?.members.removeRole(userId, role.id);
+      }
+    }
   },
 });
