3fd8218d4e0e720c5338bcb2141ab68c29d97946
kinkenkan298 <akin@programmer.net>
2025-03-03 06:13:13 +0700

    feat(welcomechannel): sudah bisa hapus channel didatabase!

--- src/commands/interactions/welcomechannel/welcome-delete.ts
+++ src/commands/interactions/welcomechannel/welcome-delete.ts
@@ -1,3 +1,5 @@
+import SetupChannelModel from "#asep/structures/schemas/guilds/SetupChannel.js";
+import { AsepEmbed } from "#asep/structures/utils/classes/AsepEmbed.js";
 import {
   CommandContext,
   createChannelOption,
@@ -27,8 +29,51 @@ export default class DeleteWelcomeCommand extends SubCommand {
     await ctx.deferReply();
     const { client, options, guildId } = ctx;
     const { channel: targetChannel } = options;
-    await ctx.editOrReply({
-      content: `channel: ${targetChannel}`,
-    });
+
+    const data = await SetupChannelModel.findOne({ guildId });
+
+    if (!data || data.channelId !== targetChannel.id) {
+      await ctx.editOrReply({
+        embeds: [
+          new AsepEmbed(
+            {
+              title: "Channel yang kamu masukan tidak ada didatabase!",
+            },
+            client,
+          ).setType("error"),
+        ],
+      });
+      return;
+    }
+
+    try {
+      await SetupChannelModel.findOneAndDelete({
+        guildId,
+        channelId: targetChannel.id,
+      });
+      await ctx.editOrReply({
+        embeds: [
+          new AsepEmbed(
+            {
+              title: "Berhasil hapus welcome channel didatabase!",
+            },
+            client,
+          ).setType("success"),
+        ],
+      });
+    } catch (e) {
+      client.logger.error(e);
+      await ctx.editOrReply({
+        embeds: [
+          new AsepEmbed(
+            {
+              title: "Terjadi kesalahan tidak terduga!",
+              description: "Silakan coba lagi beberap saat!",
+            },
+            client,
+          ).setType("error"),
+        ],
+      });
+    }
   }
 }
